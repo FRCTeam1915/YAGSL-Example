eee80fbf5074f6c6ea7fa7962af1a57483579bb7
tscofield <1007833+tscofield@users.noreply.github.com>
2024-03-08 06:09:06 -0500

    Potential fix for x buttom of doom bug

--- src/main/java/frc/robot/auto/Shooter/loadShooter.java
+++ src/main/java/frc/robot/auto/Shooter/loadShooter.java
@@ -34,15 +34,69 @@ public class loadShooter extends Command {
     public void initialize() {
     }
 
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
+    public void runmotors() {
         shooter.shooterMotorOne.set(0.1);
         shooter.shooterMotorTwo.set(-0.1);
         shooter.shooterMotorOne.setIdleMode(IdleMode.kBrake);
         shooter.shooterMotorTwo.setIdleMode(IdleMode.kBrake);
         intake.motorOne.set(ControlMode.PercentOutput, -.4);
         intake.motorTwo.set(ControlMode.PercentOutput, -.4);
+    }
+    public void stopmotors () {
+        shooter.shooterMotorOne.set(0);
+        shooter.shooterMotorTwo.set(0);
+        intake.motorOne.set(ControlMode.PercentOutput, 0);
+        intake.motorTwo.set(ControlMode.PercentOutput, 0);
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        //shooter.shooterMotorOne.set(0.1);
+        //shooter.shooterMotorTwo.set(-0.1);
+        //shooter.shooterMotorOne.setIdleMode(IdleMode.kBrake);
+        //shooter.shooterMotorTwo.setIdleMode(IdleMode.kBrake);
+        //intake.motorOne.set(ControlMode.PercentOutput, -.4);
+        //intake.motorTwo.set(ControlMode.PercentOutput, -.4);
+
+        /*
+         * trig 1 is when we encounter the first edge
+         * trig 2 is whe we encounter the hole
+         * sensor is the current sensor state
+         * 
+         *   | trig1 | trig2 | sensor |
+         *   --------------------------
+         *   |   0   |   0   |    0   |  run motors
+         *   |   0   |   0   |    1   |  keep running, set trig 1
+         *   |   0   |   1   |    0   |  NA
+         *   |   0   |   1   |    1   |  NA
+         *   |   1   |   0   |    0   |  keep running, set trig 2, we are in the hole
+         *   |   1   |   0   |    1   |  keep running 
+         *   |   1   |   1   |    0   |  keep running
+         *   |   1   |   1   |    1   |  stop motors, we are at the second part
+         * 
+         */
+
+        boolean sensor = Robot.shooterSensor.get();
+        if (trig1 == false) {
+            if (trig2 == false) {
+                runmotors();
+                if (sensor == true) {
+                    trig1 = true;
+                }
+            }
+        } else if (trig1 == true) {
+            if (trig2 == false && sensor == false) {
+                runmotors();
+                trig2 = true;
+            } else if (trig2 = true && sensor == true) {
+                stopmotors();
+                finished = true;
+            } else {
+                runmotors();
+            }
+        }
+/*
         if (Robot.shooterSensor.get() == false) {
             cycle = true;
         }
@@ -62,6 +116,7 @@ public class loadShooter extends Command {
                 intake.motorTwo.set(ControlMode.PercentOutput, 0);
             }
         }
+*/
     }
 
     // Called once the command ends or is interrupted.
@@ -69,6 +124,9 @@ public class loadShooter extends Command {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
+        stopmotors();
+        trig1 = false;
+        trig2 = false;
         return finished;
     }
 }
